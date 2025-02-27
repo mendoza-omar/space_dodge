01f4d41d25e7c4d634637901c21c7829563d86e3
mendoza-omar <84155990+mendoza-omar@users.noreply.github.com>
2025-02-26 20:08:39 -0800

    Changes up until movement

--- main.py
+++ main.py
@@ -1,21 +1,74 @@
 import pygame
 import time
 import random
+pygame.font.init()
 
 WIDTH, HEIGHT = 1000, 800
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Space Dodge")
+pygame.display.set_caption("Space Invading")
+
+BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
+
+PLAYER_WIDTH = 40
+PLAYER_HEIGHT = 60
+
+PLAYER_VEL = 5
+STAR_WIDTH = 10
+STAR_HEIGHT = 20
+STAR_VEL = 3
+
+FONT = pygame.font.SysFont("arial", 30)
+
+def draw(player, elapsed_time, stars):
+    WIN.blit(BG, (0,0))
+
+    time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "yellow")
+    WIN.blit(time_text, (10,10)
+             )
+    pygame.draw.rect(WIN, "green", player)
+
+    for star in stars:
+        pygame.draw.rect(WIN, "red", star)
+
+    pygame.display.update()
 
 def main():
     run = True
 
+    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,
+                        PLAYER_WIDTH, PLAYER_HEIGHT)
+    clock = pygame.time.Clock()
+    start_time = time.time()
+    elapsed_time = 0
+
+    star_add_increment = 2000
+    star_count = 0
+
+    stars = []
+    hit = False
+
     while run:
+        star_count += clock.tick(60)
+        elapsed_time = time.time() - start_time
+
+        if star_count > star_add_increment:
+            for _ in range(3):
+                star_x = randomw.randint(0, WIDTH - STAR_WIDTH)
+                star = pygame.Rect(star_x, - STAR_HEIGHT,
+                                   STAR_WIDTH, STAR_HEIGHT)
+                stars.append(star)
+
+            star_add_increment = max(200, star_add_increment -50)
+            star_count = 0
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
 
+        draw(player, elapsed_time, stars)    
+
     pygame.quit()            
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
